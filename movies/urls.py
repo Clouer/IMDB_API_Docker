07d219e389871fe4f1b7b8bedc1d4a5668128008
Clouer <alex.pyhtya@gmail.com>
2022-05-20 07:50:27 +0300

    serializers added

--- movies/urls.py
+++ movies/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('movie/', views.MovieListView.as_view())
+    path('movie/', views.MovieListView.as_view()),
+    path('movie/<int:pk>/', views.MovieDetailView.as_view()),
+    path('review/', views.ReviewCreateView.as_view()),
+    path('review/<int:pk>/', views.ReviewsView.as_view())
 
 ]
